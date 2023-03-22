748de93727501e1e5eaa9bc054f9a6fccc7daf4d
Blvckstone <mohammadshoiab9011@gmail.com>
2023-03-22 23:25:10 +0530

    A MongoDB native driver with working code and adding one element own developed

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-const url = "mongodb://localhost:27017";
+const url = 'mongodb://127.0.0.1:27017';
 
-const dbName = "fruitProjectDB";
+const dbName = 'fruitsDB';
 
-const client = new MongoClient(url);
+const client = new MongoClient(url, {useNewUrlParser: true});
 
-client.connect(function(err){
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-    
-    const db = client.db(dbName);   
 
-    client.close(); 
-});
+async function start(){
+    await client.connect(function(err){
+        assert.equal(null, err); 
+    })
+    const db = client.db(dbName)
+    const haiku = db.collection("haiku")
+
+    const document = {
+        fruitName: "Mango",
+        review: "Mango is good",
+    }
+
+    const result = await haiku.insertOne(document)
+    console.log(`Inserted document: _id: ${result.insertedId}`);
+
+
+    console.log("Connected Successfully to Server!");
+    await client.close();
+}
+
+start()
+
+
+
+
+// const uri = "mongodb://127.0.0.1:27017";
+// const client = new MongoClient(uri);
+// async function run() {
+//   try {
+//     await client.connect();
+
+//     await client.db("example").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
