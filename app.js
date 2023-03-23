9c6bdc3b688f90fa999e3e116be6d961684ab500
Blvckstone <mohammadshoiab9011@gmail.com>
2023-03-23 12:03:11 +0530

    client close not working other than everything working

--- app.js
+++ app.js
@@ -3,48 +3,67 @@ const assert = require('assert');
 
 const url = 'mongodb://127.0.0.1:27017';
 
-const dbName = 'fruitsDB';
+const dbName = 'fruitsDB'; // Database name to be create
 
 const client = new MongoClient(url, {useNewUrlParser: true});
 
 
-async function start(){
+//angela yu's code not working i added async function 
+
+
+async function start(){  //async function added
     await client.connect(function(err){
         assert.equal(null, err); 
     })
-    const db = client.db(dbName)
-    const haiku = db.collection("haiku")
-
-    const document = {
-        fruitName: "Mango",
-        review: "Mango is good",
-    }
-
-    const result = await haiku.insertOne(document)
-    console.log(`Inserted document: _id: ${result.insertedId}`);
-
+    const db = client.db(dbName) //creating database
 
     console.log("Connected Successfully to Server!");
-    await client.close();
-}
 
+    inserDocument(db)
+
+    
+    // await client.close();
+    
+    
+    
+    
+}
 start()
 
 
+const inserDocument = function(db){
+
+    const fruits = db.collection("fruits") //creating collections
+
+    const document = [{ // the data which we want to push in collection or database
+        fruitName: "Mango",
+        review: "Mango is good",
+    },
+    {
+        fruitName: "Apple",
+        review: "Mango is not good",
+    },
+    {
+        fruitName: "Apple",
+        review: "Mango is not good",
+    },
+]
+
+    const result = fruits.insertMany(document, function(err, result){
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log(`Inserted document: _id: ${result.insertedId}`); // logged the data that inserted
+        callback(result)
+        
+    }) // insert command or data push to database
+
+    console.log(result)
+    
+    
+}
 
 
-// const uri = "mongodb://127.0.0.1:27017";
-// const client = new MongoClient(uri);
-// async function run() {
-//   try {
-//     await client.connect();
 
-//     await client.db("example").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
 
 
