cbf85b1f2ed57ac9b8f6329fe163e7c554ae088b
Blvckstone <mohammadshoiab9011@gmail.com>
2023-03-23 14:02:04 +0530

    How to insert many in using mongoose

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/personDB", {useNewUrlParser: true});
 
 const fruitSchema = new mongoose.Schema ({
-    name: String,
+    name: String, //this should be capitalize of first letter
     rating: Number,
     review: String,
 });
 
-const Fruit = mongoose.model("Fruit", fruitSchema);
+const Fruit = mongoose.model("Fruit", fruitSchema); //Fruit is the name of collection (Singular)
 
 const fruit = new Fruit ({
     name: "Apple",
@@ -16,4 +16,32 @@ const fruit = new Fruit ({
     review: "Good Apple"
 })
 
-fruit.save();
+// fruit.save(); "this will add apples everytime when we run this script so comment it out"
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "Shoeb",
+    age: 23
+})
+
+// person.save();
+
+const kiwi = new Fruit({
+    name: "Kiwi",
+    rating: 7,
+    review: "Best Fruit Ever"
+})
+
+const banana = new Fruit({
+    name: "Banana",
+    rating: 5,
+    review: "Rich in magnecium"
+})
+
+Fruit.insertMany([kiwi, banana])
